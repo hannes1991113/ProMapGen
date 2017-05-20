519937d9be1b093d1b80c0476e78052fa2e92c25
OrangePanda <kleinh@hochschule-trier.de>
2017-05-20 15:44:24 +0200

    Erste Kombination aus Noise und Disk, aber noch sehr sketchy

--- Assets/OwnStuff/Code/DiskDistribution.cs
+++ Assets/OwnStuff/Code/DiskDistribution.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+
+
 public class DiskDistribution : MonoBehaviour {
+	public enum CombinationType {
+		NormalValue,
+		Add,
+		Multiply,
+	};
 
 	/// fot algorithm details please take a look at PoissonDiskGenerator.cs.
 	[Header("Distribution Settings")]
@@ -14,11 +21,18 @@ public class DiskDistribution : MonoBehaviour {
 	[Header("Noise Settings")]
 
 	[Header("Combination with Heightmap")]
-	public int range = 0;
+	public int maxDistance = 10;
+	public CombinationType combinationType = CombinationType.NormalValue;
+
+	public float a = 0.1f;
+	public float b = 0.7f;
+	public float c = 0.5f;
 
 	private TerrainData terrainData;
 	private float[,] heights;
 	private float[,] newHeights;
+
+
 
 	public void showPointsOnly(){
 		Generate ();
@@ -37,7 +51,7 @@ public class DiskDistribution : MonoBehaviour {
 		PoissonDiskGenerator.sampleRange = terrainData.heightmapHeight;
 	}
 
-	public void Generate(){
+	void Generate(){
 		initiate ();
 		result = PoissonDiskGenerator.Generate();
 		sampleCount = PoissonDiskGenerator.sampleCount;
@@ -52,21 +66,55 @@ public class DiskDistribution : MonoBehaviour {
 	}
 
 	public void combineWithDisks(){
-		newHeights = new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
 		Generate ();
+		newHeights = new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
 		for(int i = 0; i < sampleCount; ++i){
 			int x = (int)result [i].x;
 			int y = (int)result [i].y;
-			newHeights[x, y] = heights[x, y];
+			combineWithDisk (x, y);
+			//newHeights[x, y] = heights[x, y];
 		}
 		heights = newHeights;
 		terrainData.SetHeights (0, 0, heights);
 	}
 
 	void combineWithDisk(int middleX, int middleY){
-		int x = Mathf.Clamp(middleX - range, 0, terrainData.heightmapWidth);
-		for (; x < middleX + range || x < terrainData.heightmapWidth; x++) {
-
+		int x = Mathf.Clamp(middleX - maxDistance, 0, terrainData.heightmapWidth);
+		int xBorder = Mathf.Clamp ((middleX + maxDistance), 0, terrainData.heightmapWidth);
+		for (; x < xBorder; x++) {
+			int y = Mathf.Clamp(middleY - maxDistance, 0, terrainData.heightmapWidth);
+			int yBorder = Mathf.Clamp ((middleY + maxDistance), 0, terrainData.heightmapWidth);
+			for (; y < yBorder; y++) {
+				float distance = calculateDistance (x, y, middleX, middleY);
+				combineDistanceAndNoise (distance, x, y);
+			}
 		}
+	}
+
+	float calculateDistance(float px, float py, float qx, float qy){
+		//float distance = Mathf.Sqrt((px - qx) * (px - qx) + (py - qy) * (py - qy));
+		float distance = Vector2.Distance(new Vector2(px,py), new Vector2(qx, qy));
+		distance = distance / maxDistance;
+		//distance = Mathf.Clamp (distance, 0, 1);
+		return distance;
+	}
+
+	void combineDistanceAndNoise(float distance, int x, int y){
+		if (distance > 1)
+			return;
+		switch (combinationType){
+		case CombinationType.NormalValue:
+		default:
+			newHeights [x, y] = heights [x, y];
+			break;
+		case CombinationType.Add:
+			newHeights [x, y] = heights [x, y] + a - b * Mathf.Pow (distance, c);
+			break;
+		case CombinationType.Multiply:
+			newHeights [x, y] = (heights [x, y] + a) * (1 - b * Mathf.Pow (distance, c));
+			break;
+		}
+
+
 	}
 }
