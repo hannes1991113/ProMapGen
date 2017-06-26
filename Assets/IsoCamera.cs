7e4d31fe57d0c97408134a0db08eeb94f480495a
OrangePanda <kleinh@hochschule-trier.de>
2017-06-26 15:38:36 +0200

    Kamera aus Isoperspektive, Terrain jetzt anklickbar, Name wird im DebugLog ausgegeben

---
+++ Assets/IsoCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsoCamera : MonoBehaviour {
+
+	public float speed = 10;
+
+	private GameObject terrain;
+
+	private TerrainCollider terrainCollider;
+
+	private TerrainData terraindata;
+
+	private ProMapGen.SplatmapCreator splatmapCreator;
+
+	private Camera myCam;
+
+	// Use this for initialization
+	void Start () {
+		terrain = GameObject.Find ("Terrain");
+		terrainCollider = terrain.GetComponent<TerrainCollider> ();
+		terraindata = terrain.GetComponent<Terrain> ().terrainData;
+		splatmapCreator = terrain.GetComponent<ProMapGen.SplatmapCreator> ();
+		myCam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Input.GetAxis ("Vertical") * speed, Space.World);
+		transform.Translate (Vector3.right * Input.GetAxis ("Horizontal") * speed, Space.World);
+
+		if(Input.GetMouseButtonDown(0)){
+			checkCollission();
+		}
+	}
+	
+	void checkCollission(){
+		Ray ray = myCam.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hitInfo;
+		if (terrainCollider.Raycast (ray, out hitInfo, 9999)) {
+			Vector3 hitPosition = hitInfo.point;
+			hitPosition = terrain.transform.worldToLocalMatrix.MultiplyPoint (hitPosition);
+			hitPosition.x = (hitPosition.x / terraindata.size.x) * terraindata.heightmapResolution;
+			hitPosition.z = (hitPosition.z / terraindata.size.z) * terraindata.heightmapResolution;
+			splatmapCreator.getTileName ((int)hitPosition.z, (int)hitPosition.x);
+		}
+	}
+}
--- Assets/OwnStuff/Code/SplatmapCreator.cs
+++ Assets/OwnStuff/Code/SplatmapCreator.cs
@@ -3,102 +3,110 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace ProMapGen{
-public class SplatmapCreator : MonoBehaviour {
-	public NoiseCreator[] noises;
-	public Area[] areas;
+	public class SplatmapCreator : MonoBehaviour {
+		public NoiseCreator[] noises;
+		public Area[] areas;
 
-	public Vector2 position;
+		public Vector2 position;
 
-	[Range(0,1)]
-	float waterLevel = 0.1f;
+		[Range(0,1)]
+		float waterLevel = 0.1f;
 
-	private TerrainData terrainData;
-	private float[,] heights;
+		private TerrainData terrainData;
+		private float[,] heights;
 
-	private float[,,] splatmap;
-	private Transform water;
-	private int numOfTextures;
+		private float[,,] splatmap;
+		private Transform water;
+		private int numOfTextures;
 
-	private Tile[,] tileMap;
+		private Tile[,] tileMap;
 
-	void initiate(){
-		terrainData = GetComponent<Terrain> ().terrainData;
-		terrainData.alphamapResolution = terrainData.heightmapResolution;
-		heights = terrainData.GetHeights (0, 0, terrainData.heightmapWidth, terrainData.heightmapHeight);
-		tileMap = new Tile[terrainData.heightmapWidth, terrainData.heightmapHeight];
-		processAndCountTextures ();
-		splatmap = new float[terrainData.alphamapHeight, terrainData.alphamapWidth, numOfTextures];
-		createNoiseCreators ();
-	}
+		void Start(){
+			CreateSplatmap ();
+		}
 
-	public void CreateSplatmap()
-	{
-		initiate ();
-		for(int y = 0; y < terrainData.alphamapHeight; y++){
-			for(int x = 0; x < terrainData.alphamapWidth; x++){
-				for (int i = 0; i < areas.Length; i++) {
-					if (heights[y,x] <= areas [i].cut) {
-						Tile newTile = areas[i].getTile(y,x);
-						tileMap [y, x] = newTile;
-						splatmap [y, x, newTile.textureNumber] = 1;
-						break;
+		void initiate(){
+			terrainData = GetComponent<Terrain> ().terrainData;
+			terrainData.alphamapResolution = terrainData.heightmapResolution;
+			heights = terrainData.GetHeights (0, 0, terrainData.heightmapWidth, terrainData.heightmapHeight);
+			tileMap = new Tile[terrainData.heightmapWidth, terrainData.heightmapHeight];
+			processAndCountTextures ();
+			splatmap = new float[terrainData.alphamapHeight, terrainData.alphamapWidth, numOfTextures];
+			createNoiseCreators ();
+		}
+
+		public void CreateSplatmap()
+		{
+			initiate ();
+			for(int y = 0; y < terrainData.alphamapHeight; y++){
+				for(int x = 0; x < terrainData.alphamapWidth; x++){
+					for (int i = 0; i < areas.Length; i++) {
+						if (heights[y,x] <= areas [i].cut) {
+							Tile newTile = areas[i].getTile(y,x);
+							tileMap [y, x] = newTile;
+							splatmap [y, x, newTile.textureNumber] = 1;
+							break;
+						}
 					}
 				}
 			}
+			terrainData.SetAlphamaps (0, 0, splatmap);
 		}
-		terrainData.SetAlphamaps (0, 0, splatmap);
-	}
 
-	void processAndCountTextures(){
-		List<SplatPrototype> list = new List<SplatPrototype> ();
-		numOfTextures = 0;
-		for (int i = 0; i < areas.Length; i++) {
-			Area curArea = areas [i];
-			SplatPrototype newSP = new SplatPrototype ();
-			newSP.texture = curArea.defaultTexture;
-			list.Add (newSP);
-			curArea.TextureNumber = numOfTextures;
-			numOfTextures++;
-			for (int j = 0; j < curArea.subAreas.Length; j++) {
-				SubArea curSubArea = curArea.subAreas [j];
-				newSP = new SplatPrototype ();
-				newSP.texture = curSubArea.texture;
+		void processAndCountTextures(){
+			List<SplatPrototype> list = new List<SplatPrototype> ();
+			numOfTextures = 0;
+			for (int i = 0; i < areas.Length; i++) {
+				Area curArea = areas [i];
+				SplatPrototype newSP = new SplatPrototype ();
+				newSP.texture = curArea.defaultTexture;
 				list.Add (newSP);
-				curSubArea.TextureNumber = numOfTextures;
-				curSubArea.takeNoise (noises);
+				curArea.TextureNumber = numOfTextures;
 				numOfTextures++;
+				for (int j = 0; j < curArea.subAreas.Length; j++) {
+					SubArea curSubArea = curArea.subAreas [j];
+					newSP = new SplatPrototype ();
+					newSP.texture = curSubArea.texture;
+					list.Add (newSP);
+					curSubArea.TextureNumber = numOfTextures;
+					curSubArea.takeNoise (noises);
+					numOfTextures++;
+				}
+			}
+			SplatPrototype[] array = list.ToArray ();
+			terrainData.splatPrototypes = array;
+		}
+
+		void createNoiseCreators(){
+			foreach (NoiseCreator noise in noises) {
+				noise.create (terrainData.heightmapResolution);
+				if (GetComponent<Controller> ().debugResetRandom) {
+					Random.state = GetComponent<Controller> ().randomStartState;
+				}
+				if (GetComponent<Controller> ().debugCreateTextures) {
+					noise.createTexture ();
+				}
 			}
 		}
-		SplatPrototype[] array = list.ToArray ();
-		terrainData.splatPrototypes = array;
-	}
-
-	void createNoiseCreators(){
-		foreach (NoiseCreator noise in noises) {
-			noise.create (terrainData.heightmapResolution);
-			if (GetComponent<Controller> ().debugResetRandom) {
-				Random.state = GetComponent<Controller> ().randomStartState;
-			}
-			if (GetComponent<Controller> ().debugCreateTextures) {
-				noise.createTexture ();
-			}
+		
+		public void getTileName(int x, int y){
+			Tile posTile = tileMap [x, y];
+			Debug.Log (posTile.areaName + " and " + posTile.subAreaName + " at " + x + " " + y);
 		}
-	}
+		
+		public void getTileName(){
+			getTileName ((int)position.x, (int)position.y);
+		}
 
-	public void getTileName(){
-		Tile posTile = tileMap [(int)position.x, (int)position.y];
-		Debug.Log (posTile.areaName + " and " + posTile.subAreaName + " at " + position.ToString());
+		//Shit
+		public void setWaterLevel(){
+			water = transform.GetChild (0);
+			float scaleX = terrainData.heightmapScale.x;
+			float scaleY = terrainData.heightmapScale.y;
+			float scaleZ = terrainData.heightmapScale.z;
+			Debug.Log("ScaleX = " + scaleX + " ScaleY = " + scaleY + " ScaleZ = " + scaleZ);
+			water.localPosition = new Vector3 (water.localPosition.x , waterLevel * scaleY, water.localPosition.z);
+		}
+		
 	}
-
-	//Shit
-	public void setWaterLevel(){
-		water = transform.GetChild (0);
-		float scaleX = terrainData.heightmapScale.x;
-		float scaleY = terrainData.heightmapScale.y;
-		float scaleZ = terrainData.heightmapScale.z;
-		Debug.Log("ScaleX = " + scaleX + " ScaleY = " + scaleY + " ScaleZ = " + scaleZ);
-		water.localPosition = new Vector3 (water.localPosition.x , waterLevel * scaleY, water.localPosition.z);
-	}
-	
-}
 }
