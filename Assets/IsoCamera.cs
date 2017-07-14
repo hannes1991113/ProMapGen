7a9c7045b992925bb0c463aa0960fe684914dee9
OrangePanda <kleinh@hochschule-trier.de>
2017-07-14 22:59:27 +0200

    Ordner umstrukturiert und Prefab erstellt

--- Assets/IsoCamera.cs
+++ Assets/IsoCamera.cs
@@ -6,22 +6,10 @@ public class IsoCamera : MonoBehaviour {
 
 	public float speed = 10;
 
-	private GameObject terrain;
-
-	private TerrainCollider terrainCollider;
-
-	private TerrainData terraindata;
-
-	private ProMapGen.SplatmapCreator splatmapCreator;
-
 	private Camera myCam;
 
 	// Use this for initialization
 	void Start () {
-		terrain = GameObject.Find ("Terrain");
-		terrainCollider = terrain.GetComponent<TerrainCollider> ();
-		terraindata = terrain.GetComponent<Terrain> ().terrainData;
-		splatmapCreator = terrain.GetComponent<ProMapGen.SplatmapCreator> ();
 		myCam = GetComponent<Camera> ();
 	}
 	
@@ -37,13 +25,20 @@ public class IsoCamera : MonoBehaviour {
 	
 	void checkCollission(){
 		Ray ray = myCam.ScreenPointToRay (Input.mousePosition);
+
 		RaycastHit hitInfo;
-		if (terrainCollider.Raycast (ray, out hitInfo, 9999)) {
-			Vector3 hitPosition = hitInfo.point;
-			hitPosition = terrain.transform.worldToLocalMatrix.MultiplyPoint (hitPosition);
-			hitPosition.x = (hitPosition.x / terraindata.size.x) * terraindata.heightmapResolution;
-			hitPosition.z = (hitPosition.z / terraindata.size.z) * terraindata.heightmapResolution;
-			splatmapCreator.getTileName ((int)hitPosition.z, (int)hitPosition.x);
+		if (Physics.Raycast (ray, out hitInfo)) {
+			if (hitInfo.collider.gameObject.GetComponent<ProMapGen.Controller> ()) {
+				Vector3 hitPosition = hitInfo.point;
+				GameObject terrain = hitInfo.collider.gameObject;
+				TerrainData terraindata = terrain.GetComponent<Terrain> ().terrainData;
+				ProMapGen.SplatmapCreator splatmapCreator = terrain.GetComponent<ProMapGen.SplatmapCreator> ();
+
+				hitPosition = terrain.transform.worldToLocalMatrix.MultiplyPoint (hitPosition);
+				hitPosition.x = (hitPosition.x / terraindata.size.x) * terraindata.heightmapResolution;
+				hitPosition.z = (hitPosition.z / terraindata.size.z) * terraindata.heightmapResolution;
+				splatmapCreator.getTileName ((int)hitPosition.z, (int)hitPosition.x);
+			}
 		}
 	}
 }
