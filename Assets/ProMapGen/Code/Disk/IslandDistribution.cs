70577edde45a7188720a5e6239dc7efa30154650
OrangePanda <kleinh@hochschule-trier.de>
2017-07-16 15:25:32 +0200

    Manuelle Inselverteilung und bischen aufgeräumt

--- Assets/ProMapGen/Code/Disk/IslandDistribution.cs
+++ Assets/ProMapGen/Code/Disk/IslandDistribution.cs
@@ -4,47 +4,44 @@ using System.Collections.Generic;
 
 namespace ProMapGen{
 
-public class IslandDistribution : MonoBehaviour {
-	public enum CombinationType {
-		Add,
-		Multiply,
-		Exponent,
-	};
+	public class IslandDistribution : MonoBehaviour {
+		// minimum distance between samples
+		public float minDistance = 5.0f;
+		// number of tries, higher number get better result but slower
+		public int k = 30;
+		// manual distribution, if true ignore all members above
+		public bool manualDistribution = false;
+		// list of points
+		public List<Vector2> result;
 
-	/// for algorithm details please take a look at PoissonDiskGenerator.cs.
-	public float minDistance = 5.0f;	// minimum distance between samples.
-	public int k = 30;					// darting time. Higher number get better result but slower.
-	public int sampleCount = 0;			// number of the samples.
-	private List<Vector2> result;		// the result of sample list.
+		private TerrainData terrainData;
 
-
-	private TerrainData terrainData;
-
-	public void showPointsOnly(){
-		generateDisks ();
-		float[,] newHeightMap =  new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
-		for(int i = 0; i < sampleCount; ++i){
-			newHeightMap[(int)result[i].x, (int)result[i].y] = 1.0f;
+		void initiate(){
+			terrainData = GetComponent<Terrain> ().terrainData;
+			PoissonDiskGenerator.minDist = minDistance;
+			PoissonDiskGenerator.k = k;
+			PoissonDiskGenerator.sampleRange = terrainData.heightmapHeight;
+			Controller controller = GetComponent<Controller> ();
+			if (controller.debugResetRandom) {
+				Random.state = controller.randomStartState;
+			}
 		}
-		terrainData.SetHeights (0, 0, newHeightMap);
-	}
 
-	void initiate(){
-		terrainData = GetComponent<Terrain> ().terrainData;
-		PoissonDiskGenerator.minDist = minDistance;
-		PoissonDiskGenerator.k = k;
-		PoissonDiskGenerator.sampleRange = terrainData.heightmapHeight;
-		Controller controller = GetComponent<Controller> ();
-		if (controller.debugResetRandom) {
-			Random.state = controller.randomStartState;
+		public void showPointsOnly(){
+			generateDisks ();
+			float[,] newHeightMap =  new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
+			for(int i = 0; i < result.Count; ++i){
+				newHeightMap[(int)result[i].x, (int)result[i].y] = 1.0f;
+			}
+			terrainData.SetHeights (0, 0, newHeightMap);
+		}
+
+		public List<Vector2> generateDisks(){
+			initiate ();
+			if (!manualDistribution) {
+				result = PoissonDiskGenerator.Generate ();
+			}
+			return result;
 		}
 	}
-
-	public List<Vector2> generateDisks(){
-		initiate ();
-		result = PoissonDiskGenerator.Generate();
-		sampleCount = PoissonDiskGenerator.sampleCount;
-		return result;
-	}
-}
 }
